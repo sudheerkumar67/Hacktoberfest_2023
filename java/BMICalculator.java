1e546c4d300a4e91c3627ec415035bd344c651ee
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2023-10-06 00:48:23 +0530

    Update BMICalculator.java

--- java/BMICalculator.java
+++ java/BMICalculator.java
@@ -2,40 +2,28 @@ import java.util.Scanner;
 
 public class BMICalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("BMI Calculator\n---------------");
 
-        System.out.println("BMI Calculator");
-        System.out.println("---------------");
+            double weight = getDoubleInput(scanner, "Enter your weight in kilograms: ");
+            double height = getDoubleInput(scanner, "Enter your height in meters: ");
 
-        System.out.print("Enter your weight in kilograms: ");
-        double weight = scanner.nextDouble();
+            double bmi = weight / (height * height);
 
-        System.out.print("Enter your height in meters: ");
-        double height = scanner.nextDouble();
-
-        double bmi = calculateBMI(weight, height);
-
-        System.out.println("Your BMI is: " + bmi);
-        System.out.println("BMI Category: " + getBMICategory(bmi));
-
-        scanner.close();
-    }
-
-    // Calculate BMI
-    public static double calculateBMI(double weight, double height) {
-        return weight / (height * height);
-    }
-
-    // Determine BMI category
-    public static String getBMICategory(double bmi) {
-        if (bmi < 18.5) {
-            return "Underweight";
-        } else if (bmi >= 18.5 && bmi < 24.9) {
-            return "Normal Weight";
-        } else if (bmi >= 25 && bmi < 29.9) {
-            return "Overweight";
-        } else {
-            return "Obese";
+            System.out.println("Your BMI is: " + bmi);
+            System.out.println("BMI Category: " + getBMICategory(bmi));
         }
+    }
+
+    private static double getDoubleInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextDouble();
+    }
+
+    private static String getBMICategory(double bmi) {
+        if (bmi < 18.5) return "Underweight";
+        if (bmi < 24.9) return "Normal Weight";
+        if (bmi < 29.9) return "Overweight";
+        return "Obese";
     }
 }
